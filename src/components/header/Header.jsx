0a95ae3a2082a8fe762d2f2b12a2954a80187e2c
adarshgowdaa <adarshgowda463@gmail.com>
2023-06-25 18:04:55 +0530

    Updated HeroBanner

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './style.scss'
+
+function header() {
+  return (
+    <div>
+      Header
+    </div>
+  )
+}
+
+export default header
---
+++ src/pages/details/Details.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './style.scss'
+
+function Details() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Details
